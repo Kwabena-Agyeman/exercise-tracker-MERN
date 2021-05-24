6393297e66a264a57634abfb67185fd82977197e
Kwabena Agyeman <kagyeman@ymail.com>
2021-05-24 15:30:49 +0000

    addedd mongoose models

---
+++ backend/models/exercise.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ExerciseSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+
+module.exports = Exercise;
